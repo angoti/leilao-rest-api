53cd0e6121f4db8b49b31b263371ab1c460bd5d5
Edson Angoti <angoti@gmail.com>
2021-05-03 14:11:38 -0300

    Project created

---
+++ src/main/java/com/professorangoti/leilao/controller/ParticipanteController.java
@@ -0,0 +1,49 @@
+package com.professorangoti.leilao.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.professorangoti.leilao.domain.Participante;
+import com.professorangoti.leilao.service.ParticipanteService;
+
+@RestController
+@RequestMapping("/participante")
+public class ParticipanteController {
+
+	@Autowired
+	private ParticipanteService service;
+
+	@GetMapping("/{id}")
+	public Participante lance(@PathVariable Integer id) {
+		return service.findById(id);
+	}
+
+	@GetMapping
+	public List<Participante> lances() {
+		return service.todos();
+	}
+
+	@PostMapping
+	public Participante novo(@RequestBody Participante entity) {
+		return service.save(entity);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Integer id) {
+		service.delete(id);
+	}
+
+	@PutMapping("/{id}")
+	public Participante atualiza(@PathVariable Integer id, @RequestBody Participante entity) {
+		return service.atualiza(id, entity);
+	}
+}
---
+++ src/main/java/com/professorangoti/leilao/service/ItemDeLeilaoService.java
@@ -0,0 +1,39 @@
+package com.professorangoti.leilao.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.professorangoti.leilao.domain.ItemDeLeilao;
+import com.professorangoti.leilao.repository.ItemDeLeilaoRepository;
+
+@Service
+public class ItemDeLeilaoService {
+
+	@Autowired
+	private ItemDeLeilaoRepository repository;
+
+	public ItemDeLeilao findById(Integer id) {
+		return repository.findById(id).orElseThrow(() -> new LanceNaoEncontradoException(id));
+	}
+
+	public List<ItemDeLeilao> todos() {
+		return repository.findAll();
+	}
+
+	public ItemDeLeilao save(ItemDeLeilao entity) {
+		return repository.save(entity);
+	}
+
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+
+	public ItemDeLeilao atualiza(Integer id, ItemDeLeilao entity) {
+		entity.setId(id);
+		return repository.save(entity);
+	}
+
+}
+
